e78e8c3ced233e77ae018c651ca767ea8ce6bc27
Rigote <matheus.rigote@gmail.com>
2020-11-29 03:52:00 -0300

    Config Storybook absolute path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs/register'],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 };
