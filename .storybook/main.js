b5a49f996bb66c3b71fd4dea8532e50f093ecdbf
Rigote <matheus.rigote@gmail.com>
2020-12-02 02:27:45 -0300

    Criando componente logo + testes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs/register'],
+  addons: ['@storybook/addon-essentials'],
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
